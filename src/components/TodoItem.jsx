8c198e5407b40f257401c215497f813bbdce61b8
masrudmubarok <masrud.mubarok21@gmail.com>
2025-02-01 12:11:41 +0700

    Check button on todo item

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import style from '../styles/modules/todoItem.module.scss';
 import { getClasses } from '../utils/getClasses';
 import { format } from 'date-fns';
 import { MdDelete, MdEdit } from 'react-icons/md';
 import { useDispatch } from 'react-redux';
-import { deleteTodo } from '../slice/todoSlice';
+import { deleteTodo, updateTodo } from '../slice/todoSlice';
 import TodoModal from './TodoModal';
+import CheckButton from './CheckButton';
 
 function TodoItem({ todo }) {
     const dispatch = useDispatch();
+    const [checked, setChecked] = useState(false);
     const [updateModalOpen, setUpdateModalOpen] = useState(false);
+
+    useEffect(() => {
+        if (todo.status === 'complete') { 
+            setChecked(true);
+        } else {
+            setChecked(false);
+        }
+    }, [todo.status]);
+
+    const handleCheck = () => {
+        setChecked(!checked);
+        dispatch(
+            updateTodo({ ...todo, status: checked ? 'incomplete' : 'complete'})
+        );
+    };
 
     const handleDelete = () => {
         dispatch(deleteTodo(todo.id));
@@ -19,53 +36,53 @@ function TodoItem({ todo }) {
         setUpdateModalOpen(true);
     };
 
-  return (
-    <>
-        <div className={style.item}>
-            <div className={style.todoDetails}>
-                [ ]
-                <div className={style.texts}>
-                    <p className={getClasses([
-                        style.todoText, 
-                        todo.status == 'complete' && style['todoText--completed'],
-                        ])}
+    return (
+        <>
+            <div className={style.item}>
+                <div className={style.todoDetails}>
+                    <CheckButton checked={checked} handleCheck={handleCheck} />
+                    <div className={style.texts}>
+                        <p className={getClasses([
+                            style.todoText, 
+                            todo.status == 'complete' && style['todoText--completed'],
+                            ])}
+                        >
+                            {todo.title}
+                        </p>
+                        <p className={style.time}>
+                            {format(new Date(todo.time), 'p, MM/dd/yyyy')}
+                        </p>
+                    </div>
+                </div>
+                <div className={style.todoActions}>
+                    <div 
+                        className={style.icon} 
+                        onClick={handleDelete} 
+                        onKeyDown={handleDelete} 
+                        role="button" 
+                        tabIndex={0}
                     >
-                        {todo.title}
-                    </p>
-                    <p className={style.time}>
-                        {format(new Date(todo.time), 'p, MM/dd/yyyy')}
-                    </p>
+                        <MdDelete />
+                    </div>
+                    <div 
+                        className={style.icon} 
+                        onClick={handleEdit} 
+                        onKeyDown={handleEdit} 
+                        role="button" 
+                        tabIndex={0}
+                    >
+                        <MdEdit />
+                    </div>
                 </div>
             </div>
-            <div className={style.todoActions}>
-                <div 
-                    className={style.icon} 
-                    onClick={handleDelete} 
-                    onKeyDown={handleDelete} 
-                    role="button" 
-                    tabIndex={0}
-                >
-                    <MdDelete />
-                </div>
-                <div 
-                    className={style.icon} 
-                    onClick={handleEdit} 
-                    onKeyDown={handleEdit} 
-                    role="button" 
-                    tabIndex={0}
-                >
-                    <MdEdit />
-                </div>
-            </div>
-        </div>
-        <TodoModal 
-            type='update'
-            todo={todo}
-            modalOpen={updateModalOpen}
-            setModalOpen={setUpdateModalOpen}
-        />
-    </>
-  )
+            <TodoModal 
+                type='update'
+                todo={todo}
+                modalOpen={updateModalOpen}
+                setModalOpen={setUpdateModalOpen}
+            />
+        </>
+    )
 }
 
 export default TodoItem
